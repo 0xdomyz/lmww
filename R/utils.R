ccfec6df63e8748e91b99be1b914b29a58115cb1
0xdomyz <yzdominic@outlook.com>
2024-01-21 18:38:53 +1100

    add util funcs

--- R/utils.R
+++ R/utils.R
@@ -14,54 +14,57 @@ std <- function(v) {
     (v - mean(v, na.rm = TRUE)) / sqrt(stats::var(v, na.rm = TRUE))
 }
 
+
+#' Standardize a dataframe to get a parameters list
 #'
-#' #' Standardize a numeric vector to get a parameters list
-#' #'
-#' #' @param data, numeric vector, NA allowed.
-#' #'
-#' #' @return list with 3 defined columns.
-#' #' @export
-#' #'
-#' #' @examples
-#' #' a <- std_fit(mtcars)
-#' #' a$means
-#' #' a$stds
-#' std_fit <- function(data) {
-#'     means <- purrr::map_df(data, function(v) mean(v, na.rm = TRUE))
-#'     stds <- purrr::map_df(data, function(v) sqrt(stats::var(v, na.rm = TRUE)))
+#' @param data dataframe of numeric columns.
 #'
-#'     return(list(
-#'         "variables" = colnames(means),
-#'         "means" = means,
-#'         "stds" = stds
-#'     ))
-#' }
+#' @return list with 3 defined values.
+#'  - variables: a character vector of column names.
+#' - means: a numeric vector of means.
+#' - stds: a numeric vector of standard deviations.
+#' @export
 #'
+#' @examples
+#' a <- std_fit(mtcars)
+#' a$means
+#' a$stds
+std_fit <- function(data) {
+    means <- purrr::map_df(data, function(v) mean(v, na.rm = TRUE))
+    stds <- purrr::map_df(data, function(v) sqrt(stats::var(v, na.rm = TRUE)))
+
+    return(list(
+        "variables" = colnames(means),
+        "means" = means,
+        "stds" = stds
+    ))
+}
+
+
+#' Standardize a numeric vector, using a list of standardization parameters
 #'
-#' #' Standardize a numeric vector, using a list of standardization parameters
-#' #'
-#' #' The list are expected to be generated from the std_fit function.
-#' #'
-#' #' @param parameters, list
-#' #' @param data
-#' #'
-#' #' @return
-#' #' @export
-#' #'
-#' #' @examples
-#' #' std_paras <- std_fit(mtcars[c("hp", "mpg", "vs")])
-#' #'
-#' #' std_transform(std_paras, mtcars) |> summary()
-#' #' std_transform(std_paras, rbind(mtcars, mtcars)) |> summary()
-#' #' std_transform(std_paras, mtcars[c("vs", "mpg")]) |> summary()
-#' #' std_transform(std_paras, mtcars[c("carb", "wt")]) |> summary()
-#' std_transform <- function(parameters, data) {
-#'     common_var <- intersect(colnames(data), parameters$variables)
-#'     data <- data[common_var]
-#'     means <- as.numeric(parameters$means[common_var])
-#'     stds <- as.numeric(parameters$stds[common_var])
-#'     purrr::pmap(
-#'         list(data, means, stds),
-#'         function(v, m, std) (v - m) / std
-#'     ) |> dplyr::bind_cols()
-#' }
+#' The list is expected to be generated from the std_fit function.
+#'
+#' @param parameters list competible with std_fit return value.
+#' @param data dataframe of numeric columns.
+#'
+#' @return dataframe.
+#' @export
+#'
+#' @examples
+#' std_paras <- std_fit(mtcars[c("hp", "mpg", "vs")])
+#'
+#' std_transform(std_paras, mtcars) |> summary()
+#' std_transform(std_paras, rbind(mtcars, mtcars)) |> summary()
+#' std_transform(std_paras, mtcars[c("vs", "mpg")]) |> summary()
+#' std_transform(std_paras, mtcars[c("carb", "wt")]) |> summary()
+std_transform <- function(parameters, data) {
+    common_var <- intersect(colnames(data), parameters$variables)
+    data <- data[common_var]
+    means <- as.numeric(parameters$means[common_var])
+    stds <- as.numeric(parameters$stds[common_var])
+    purrr::pmap(
+        list(data, means, stds),
+        function(v, m, std) (v - m) / std
+    ) |> dplyr::bind_cols()
+}
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -7,9 +7,38 @@ test_that("std", {
 })
 
 test_that("std_fit", {
+  a <- std_fit(mtcars)
+
   expect_equal(
-    1 + 1,
-    2,
+    as.numeric(a$means),
+    c(20.090625, 6.1875, 230.721875, 146.6875, 3.5965625, 3.21725, 17.84875, 0.4375, 0.40625, 3.6875, 2.8125),
+    tolerance = 1e-7
+  )
+  expect_equal(
+    as.numeric(a$stds),
+    c(6.0269480520891, 1.78592164694654, 123.938693831382, 68.5628684893206, 0.534678736070971, 0.978457442989697, 1.78694323609684, 0.504016128774185, 0.498990917235846, 0.737804065256947, 1.61519997763185),
+    tolerance = 1e-7
+  )
+  expect_equal(
+    a$variables,
+    c("mpg", "cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb"),
+    tolerance = 1e-7
+  )
+})
+
+test_that("std_transform", {
+  std_paras <- std_fit(mtcars)
+  stded_col <- std_transform(std_paras, mtcars)["mpg"][[1]]
+
+  expect_equal(
+    mean(stded_col),
+    0,
+    tolerance = 1e-7
+  )
+
+  expect_equal(
+    var(stded_col),
+    1,
     tolerance = 1e-7
   )
 })
